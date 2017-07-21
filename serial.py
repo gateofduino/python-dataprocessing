94ba20021170fce966b5ae7a483dc85d37baf093
gateofduino <30337708+gateofduino@users.noreply.github.com>
2017-07-21 11:19:27 +0300

    Update serial.py

--- serial.py
+++ serial.py
@@ -1,7 +1,7 @@
-import serial
+import serial //PySeial kütüphanesini ekleyin
 
-ser = serial.Serial('COM3', baudrate = 9600, timeout =1)
+ser = serial.Serial('COM3', baudrate = 9600, timeout =1) //COM BAUDRATE ayarla, bağlantı kur.
 
 while 1:
-    arduinoData = ser.readline()
-    print(arduinoData)
+    arduinoData = ser.readline()        //Gelen verileri al
+    print(arduinoData) //Verileri yazdır
